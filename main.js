41a6842815084d16fc89cb5e4552c9cd43a53e66
Fig <bethanytrr@gmail.com>
2022-12-15 13:14:07 -0500

    Cypress tests pass.

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+const cellChanger = document.querySelectorAll ("cell")
+const colorPicker = document.querySelectorAll(".color")
+const currentColor = document.querySelector("#current-color")
+
+for (let color of colorPicker){
+  color.addEventListener("click",(event)=> {
+    color.style.background = currentColor.style.background
+  })
+}
+
+for (let cell of cellChanger){
+  cell.addEventListener("click", (event) => {
+    cell.style.background = currentColor.style.background
+  })
+}
